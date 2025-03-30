4de592751a88f41adebe854c9d148ab263439d67
leonidas <leonidasmoreira098@gmail.com>
2025-03-30 15:16:46 -0300

    criação dos repositories para acesso ao banco de dados

---
+++ src/main/java/com/example/helpdesk_api/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.example.helpdesk_api.repository;
+
+import com.example.helpdesk_api.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
